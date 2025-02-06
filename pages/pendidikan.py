40c4274b72c874698405c3554c8de294728264ec
Anung Ramadhan <anungrmdhn@gmail.com>
2025-02-06 18:12:49 +0700

    tamah data SMK

--- pages/pendidikan.py
+++ pages/pendidikan.py
@@ -3,7 +3,10 @@ from controllers.pendidikan_controller import df_pendidikan_smk
 
 st.title("DATA PENDIDIKAN BNA")
 
-
+st.markdown("""
+##### DATA SMK BANJARNEGARA 2023
+###### Sumber: BPS BANJARNEGARA 2023
+""")
 st.dataframe(df_pendidikan_smk)
 
 
